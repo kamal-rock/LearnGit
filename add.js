7c36516a7fae6669db0d95d842800d27878476b1
kamal kumar <kamalnitw157227@gmail.com>
2022-08-03 22:01:10 +0530

    adding add.js

---
+++ add.js
@@ -0,0 +1,30 @@
+//static
+
+// let a =5;
+// let b=10;
+// let c =a+b;
+// console.log('sum of a and b is:' +c);
+
+
+
+//dynamic data
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+
+//     <script>
+//         let a = prompt('enter the number a','');
+// let b= prompt('enter the number b','');
+// let c= parseInt(a) + parseInt(b);
+// console.log('sum of a and b is:' +c);
+//     </script>
+// </head>
+// <body>
+    
+// </body>
+// </html>
