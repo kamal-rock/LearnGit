82497276044b0152d728826fc04c666fde0f0a6a
kamal kumar <kamalnitw157227@gmail.com>
2022-08-04 00:58:39 +0530

    added area.js

---
+++ area.js
@@ -0,0 +1,13 @@
+// var b=10;
+// var h =5;
+// const area = (b*h)/2;
+// console.log(area); 
+
+
+var a =10;
+var b=5;
+var c =10;
+var s =(a+b+c)/2;
+const area = s*(s-a)*(s-b)*(s-c);
+const temp = Math.sqrt(area);
+console.log(temp);
